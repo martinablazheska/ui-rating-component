2ede358caca659fed591fcd3a364f56589d740ad
Martina Blazheska <123024212+martinablazheska@users.noreply.github.com>
2024-06-13 13:34:39 +0200

    Frontend implementation of rating component: - Added hovering functionality - when the user hovers, the current state is displayed - Added a tooltip that shows the value of the rating

--- src/components/RatingCard.jsx
+++ src/components/RatingCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import greyStar from "/assets/grey_star.svg";
 import goldStar from "/assets/gold_star.svg";
@@ -9,10 +9,17 @@ import setRating from "../utils/setRating";
 
 import classes from "./RatingCard.module.scss";
 
-const ratings = [1, 2, 3, 4, 5];
+const ratings = [
+  { value: 1, description: "very poor" },
+  { value: 2, description: "poor" },
+  { value: 3, description: "okay" },
+  { value: 4, description: "very good" },
+  { value: 5, description: "excellent" },
+];
 
 const RatingCard = () => {
   const [currentRating, setCurrentRating] = useState(getRating());
+  const [hoveredRating, setHoveredRating] = useState(null);
 
   const handleRating = (value) => {
     if (value === currentRating) {
@@ -24,23 +31,39 @@ const RatingCard = () => {
     }
   };
 
+  const handleMouseOver = (value) => {
+    setHoveredRating(value);
+  };
+
+  const handleMouseOut = () => {
+    setHoveredRating(null);
+  };
+
+  const activeRating = hoveredRating ? hoveredRating : currentRating;
+
   return (
     <div className={classes.card}>
       <h1>Please Rate</h1>
       <div className={classes.rating}>
         {ratings.map((rating) => (
-          <img
-            key={rating}
-            src={
-              currentRating !== null
-                ? currentRating >= rating
-                  ? goldStar
+          <div>
+            <img
+              key={rating.value}
+              src={
+                activeRating !== null
+                  ? activeRating >= rating.value
+                    ? goldStar
+                    : greyStar
                   : greyStar
-                : greyStar
-            }
-            onClick={() => handleRating(rating)}
-            onMouse
-          />
+              }
+              onClick={() => handleRating(rating.value)}
+              onMouseOver={() => handleMouseOver(rating.value)}
+              onMouseOut={handleMouseOut}
+            />
+            {hoveredRating === rating.value && (
+              <span>{rating.description}</span>
+            )}
+          </div>
         ))}
       </div>
       <p>Your Feedback is Very Important To Us.</p>
